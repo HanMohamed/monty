0af373e6f0c444d1de30c63231db63b7a35bc030
ahmedehab0 <ahmed.eh01@gmail.com>
2023-08-23 19:17:57 -0400

    might revert it

--- monty.c
+++ monty.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
 	char *buffer = NULL, *error, **token = NULL;
 	int line_number = 0, i = 0, op_handler = 0;
 	stack_t *stack = NULL;
+	size_t n = 0;
 
 	if (argc != 2)
 	{
@@ -27,7 +28,7 @@ int main(int argc, char *argv[]) {
 		write(STDERR_FILENO,error, strlen(error));
 		exit(EXIT_FAILURE);
 	}
-	while(fgets(buffer, sizeof(buffer), monty_file))
+	while(getline(&buffer, &n, monty_file))
 	{
 		line_number++;
 		token[i] = strtok(buffer, " /n/t");
@@ -55,7 +56,7 @@ int main(int argc, char *argv[]) {
 	}
 	fclose(monty_file);
 	free_stack(stack);
-	free(token);
+	free(buffer);
 	return 0;
 }
 /**
